93f2e5d63e78cf866c320bee147659ab256da7ab
WanHrust <rustam.alashrafov@gmail.com>
2013-12-17 00:45:33 +0400

    failed

--- QuestionaBank/src/QuestionFrame.java
+++ QuestionaBank/src/QuestionFrame.java
@@ -2,10 +2,6 @@ import java.awt.BorderLayout;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-<<<<<<< HEAD
-=======
-import java.io.IOException;
->>>>>>> branch 'master' of ssh://git@github.com/WanHrust/QuestionBank.git
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
@@ -15,24 +11,17 @@ import javax.swing.JTextArea;
 import javax.swing.JTextField;
 import javax.swing.border.EmptyBorder;
 
-<<<<<<< HEAD
+
 public class QuestionFrame extends JFrame implements ActionListener {
-=======
-public class QuestionFrame extends JFrame implements ActionListener{
 
->>>>>>> branch 'master' of ssh://git@github.com/WanHrust/QuestionBank.git
 	private JPanel contentPane;
-<<<<<<< HEAD
-	private JLabel lblTest;
-	private JButton bNext;
 
-=======
 	private JLabel lblQuestion;
 	private JButton bTrue;
 	private JButton bFalse;
 	private JButton bSkip;
 	private int counter = 0;
->>>>>>> branch 'master' of ssh://git@github.com/WanHrust/QuestionBank.git
+
 	/**
 	 * Launch the application.
 	 */
@@ -54,21 +43,11 @@ public class QuestionFrame extends JFrame implements ActionListener{
 		setBounds(0, 0, Global.SCREEN_WIDTH, Global.SCREEN_HEIGHT);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-<<<<<<< HEAD
-		contentPane.setLayout(new GridLayout(2, 1));
-=======
+
 		contentPane.setLayout(new GridLayout(4, 1));
->>>>>>> branch 'master' of ssh://git@github.com/WanHrust/QuestionBank.git
+
 		setContentPane(contentPane);
-<<<<<<< HEAD
 
-		lblTest = new JLabel();
-		lblTest.setText(ShareData.userFisrtName + " " + ShareData.questionNO);
-		contentPane.add(lblTest);
-
-		bNext = new JButton("Next");
-		bNext.addActionListener(this);
-		contentPane.add(bNext);
 	}
 
 	@Override
@@ -76,12 +55,7 @@ public class QuestionFrame extends JFrame implements ActionListener{
 		// TODO Auto-generated method stub
 		String action = e.getActionCommand();
 		switch (action) {
-		case "Next":
-			lblTest.setText(ShareData.userFisrtName);
-			break;
-		default:
 
-=======
 		
 		try {
 			lblQuestion = new JLabel(new QuestionAnswerHolder().getRandomQuestion());
@@ -104,19 +78,5 @@ public class QuestionFrame extends JFrame implements ActionListener{
 		contentPane.add(bSkip);
 	}
 
-	@Override
-	public void actionPerformed(ActionEvent e) {
-		// TODO Auto-generated method stub
-		if (counter<ShareData.questionNO){
-			try {
-				lblQuestion.setText(new QuestionAnswerHolder().getRandomQuestion());
-			} catch (IOException e1) {
-				// TODO Auto-generated catch block
-				e1.printStackTrace();
-			}
-			counter++;
->>>>>>> branch 'master' of ssh://git@github.com/WanHrust/QuestionBank.git
-		}
-	}
 
 }
